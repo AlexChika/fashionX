dd3a36f6955edef04b716dad492fb9ad48832b8b
Alex Chika <mcluckey1@gmail.com>
2022-02-02 03:40:56 +0100

    making progress

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const root = select(":root") || document.documentElement;
 const float = select("#float");
+const floatBtn = select("#float-btn");
 const nav = select("nav");
+const floatBdy = document.querySelectorAll(".flt-bdy");
+const floatHdr = document.querySelectorAll(".fl-hdr");
 const hm_crsEl = Array.from(document.querySelectorAll("#hm-crs a"));
+const heroTxt = select(".hero-text");
+
 let crsEl = 0;
 // ...............    utils   .........
 function select(x) {
@@ -16,8 +22,10 @@ window.addEventListener("scroll", (x) => {
   const navHeight = nav.getBoundingClientRect().height;
   if (window.scrollY >= navHeight) {
     nav.classList.add("static");
+    root.style.setProperty("--nav-color", "white");
   } else {
     nav.classList.remove("static");
+    root.style.setProperty("--nav-color", "red");
   }
 });
 // Home Carousel
@@ -35,9 +43,21 @@ function scroll() {
   }
   setTimeout(() => {
     scroll();
-  }, 3000);
+  }, 4000);
 }
 scroll();
+//   hero
+const heroText = ["Fashion", "Gadgets", "Ladies-wear", "Mens-wear", "Sneakers"];
+function setHeroText() {
+  let i = 0;
+  return setInterval(() => {
+    heroTxt.textContent = heroText[i];
+    if (++i >= heroText.length) i = 0;
+  }, 3000);
+}
+setHeroText();
+//  end of hero
+
 // Floating Icon
 function drag_start(event) {
   let x = float.offsetLeft - event.clientX;
@@ -56,10 +76,23 @@ function drop(event) {
   float.style.top = event.clientY + parseInt(y) + "px";
   return false;
 }
-float.addEventListener("dragstart", drag_start);
+function showFloat(e) {
+  float.classList.toggle("show");
+}
 document.body.addEventListener("dragover", drag_over);
 document.body.addEventListener("drop", drop);
-
+floatBtn.addEventListener("click", showFloat);
+float.addEventListener("dragstart", drag_start);
+floatHdr.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    floatHdr.forEach((x) => {
+      if (x !== el) {
+        x.parentElement.classList.remove("show");
+      }
+    });
+    el.parentElement.classList.toggle("show");
+  });
+});
 // to be used codes
 /*
   console.log(event.target);
