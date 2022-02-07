e6f2ebba372c5def311346ef8509f5d174973bae
Alex Chika <mcluckey1@gmail.com>
2022-02-07 17:53:40 +0100

    making progress

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ const floatBtn = select("#float-btn");
 const floatHdr = document.querySelectorAll(".fl-hdr");
 const headers = Array.from(document.querySelectorAll(".section > .hdng"));
 const sectionBody = document.querySelectorAll(".sec-bdy");
-const cartBody = document.querySelectorAll(".crt-bdy");
-const addToCart = document.querySelectorAll(".tray");
+const cartBody = select(".crt-bdy");
 const itmState = document.querySelectorAll(".stats");
 const crtState = select("#crt-status");
-const subTotal = select("#sub-total");
-const remove = select("#crt-rm");
-const add = select("#crt-add");
-const itmNum = select("#crt-amt");
+// const subTotal = select("#sub-total");
+// const remove = select("#crt-rm");
+// const add = select("#crt-add");
+// const itmNum = select("#crt-amt");
 const crtNum = select("#cart-no");
-const min = select("#crt-min");
+// const min = select("#crt-min");
 const total = select("#crt-total");
 const clearCrt = select("#clr-crt");
 const hm_crsEl = Array.from(document.querySelectorAll("#hm-crs a"));
 const heroTxt = select(".hero-text");
 let crsEl = 0;
+
 // ...............    utils   .........
+// gets dom elements
 function select(x) {
   const el = document.querySelector(x);
   if (el) {
@@ -35,6 +36,22 @@ function select(x) {
   } else {
     throw new Error(`${x} not found`);
   }
+}
+// query a specific element
+function getEl(x, y) {
+  const el = x.querySelector(y);
+  if (el) {
+    return el;
+  }
+}
+// filters and returnsan array
+function filter(arr, filter) {
+  const array = arr.filter((data) => {
+    if (data.category === filter) {
+      return data;
+    }
+  });
+  return array;
 }
 class Products {
   async getProducts() {
@@ -55,14 +72,6 @@ class Products {
     }
   }
 }
-function filter(arr, filter) {
-  const array = arr.filter((data) => {
-    if (data.category === filter) {
-      return data;
-    }
-  });
-  return array;
-}
 class UpdateDom {
   render(products, element) {
     const domproducts = products
@@ -75,7 +84,7 @@ class UpdateDom {
           </div>
           <div class="itm-desc">
             <p class="itm-name">${name}</p>
-            <p class="">${desc.substr(0, 55)}...</p>
+            <p class="itm-dtls">${desc.substr(0, 55)}...</p>
             <p class="itm-prc">
               <span><i class="bi bi-currency-dollar"></i></span>${price}
             </p>
@@ -89,27 +98,164 @@ class UpdateDom {
       `;
       })
       .join("");
+
     element.innerHTML = domproducts;
+    element.style.display = "grid";
+    element.parentElement.querySelector(".loader-con").style.display = "none";
+  }
+  getBtns() {
+    document.querySelectorAll(".tray").forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        if (e.currentTarget.dataset.stat !== "in") {
+          const name = getEl(btn.parentElement, ".itm-name").textContent;
+          const desc = getEl(btn.parentElement, ".itm-dtls").textContent;
+          const img = getEl(btn.parentElement, "img").getAttribute("src");
+          const id = e.currentTarget.dataset.id;
+          const price = getEl(btn.parentElement, ".itm-prc").textContent;
+          const amt = 1;
+          const subtotal = "";
+          const item = { name, img, id, price, amt, desc, subtotal };
+          Storage.setCartItems(item);
+          this.cartRender(Storage.getCart());
+          this.cartValues();
+          e.currentTarget.classList.add("added");
+          e.currentTarget.dataset.stat = "in";
+          select("#cart-con").classList.add("close");
+          e.currentTarget.querySelector(".stats").textContent = "In Cart";
+        }
+      });
+    });
+  }
+  cartRender(products) {
+    const domproducts = products
+      .map((data) => {
+        const { amt, id, img, name, price, desc, subtotal } = data;
+        return `
+      <article data-id="${id}">
+          <div class="crt-item-con">
+            <div class="crt-img">
+              <img class="f-wh" src="${img}" alt="${name}" />
+            </div>
+            <div class="crt-dtls">
+              <p class="crt-item-desc">${name}</p>
+              <p class="crt-item-desc">${desc}</p>
+              <p>
+                Sub-Total:<i class="bi bi-currency-dollar"></i
+                ><span id="price">${price}</span>
+              </p>
+              <p>
+                Sub-Total:<i class="bi bi-currency-dollar"></i
+                ><span id="sub-total">${subtotal}</span>
+              </p>
+            </div>
+          </div>
+          <div class="crt-math">
+            <span id="crt-rm"> Remove<i class="bi bi-trash"></i> </span>
+            <div>
+              <span id="crt-add">
+                <i class="bi bi-plus-circle"></i>
+              </span>
+              <span id="crt-amt" class="cr">${amt}</span>
+              <span id="crt-min">
+                <i class="bi bi-dash-circle"></i>
+              </span>
+            </div>
+          </div>
+        </article>
+      `;
+      })
+      .join("");
+    cartBody.innerHTML = domproducts;
+    const plusBtn = document.querySelectorAll("#crt-add");
+    const minBtn = document.querySelectorAll("#crt-min");
+    plusBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        console.log("I was increased");
+        const amt = getEl(
+          btn.parentElement.parentElement.parentElement,
+          "#crt-amt"
+        );
+        const priceEl = getEl(
+          btn.parentElement.parentElement.parentElement,
+          "#price"
+        );
+        const sub = getEl(
+          btn.parentElement.parentElement.parentElement,
+          "#sub-total"
+        );
+        let newSubTotal = 0;
+        let amount = parseInt(amt.textContent);
+        let price = parseInt(priceEl.textContent);
+        newSubTotal += price;
+        amt.textContent = amount++;
+        sub.textContent = newSubTotal;
+        this.cartValues();
+      });
+    });
+    // minBtn.forEach((btn) => {
+    //   btn.addEventListener("click", () => {
+    //     console.log("I was decreased");
+    //     const ambtn = getEl(btn.parentElement, "#crt-amt");
+    //     // let sum =
+    //   });
+    // });
+  }
+  cartValues() {
+    let cart = Storage.getCart();
+    let sum = 0;
+    let subTotal = 0;
+    cart.forEach((item) => {
+      sum += parseInt(item.price);
+    });
+    cart.forEach((item) => {
+      subTotal += parseInt(item.amt);
+    });
+    crtState.textContent = `${subTotal} Items In Cart`;
+    crtNum.textContent = subTotal;
+    total.textContent = sum;
+  }
+  increment(btn) {
+    this.cartValues();
+  }
+  decrement(btn) {}
+  remove() {
+    console.log("I removed someone");
+  }
+}
+class Storage {
+  static setCartItems(obj) {
+    let cart;
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    } else {
+      cart = [];
+    }
+    cart.push(obj);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  static getCart() {
+    const array = JSON.parse(localStorage.getItem("cart"));
+    return array;
   }
 }
 window.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
   const updateDom = new UpdateDom();
-  products.getProducts().then((products) => {
-    sectionBody.forEach((section) => {
-      updateDom.render(filter(products, section.dataset.id), section);
+
+  products
+    .getProducts()
+    .then((products) => {
+      sectionBody.forEach((section) => {
+        updateDom.render(filter(products, section.dataset.id), section);
+      });
+    })
+    .then(() => {
+      console.log("third layer");
+      updateDom.getBtns();
+    })
+    .catch((error) => {
+      // window.location.reload();
     });
-  });
-});
-addToCart.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    if (e.currentTarget.dataset.stat !== "in") {
-      e.currentTarget.classList.add("added");
-      e.currentTarget.dataset.stat = "in";
-      select("#cart-con").classList.add("close");
-      e.currentTarget.querySelector(".stats").textContent = "In Cart";
-    }
-  });
 });
 // Nav and cart buttons
 window.addEventListener("scroll", (x) => {
